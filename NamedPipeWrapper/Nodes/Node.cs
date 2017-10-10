fea2a490bcee313d4ccfe40815295076188464b3
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-10-10 20:00:14 +0200

    Add Nodes
    * Add Node and Node subnamespace

---
+++ NamedPipeWrapper/Nodes/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedPipeWrapper.Nodes
+{
+    public class Node : IDisposable
+    {
+        MemoryMappedFile memory;
+
+        public Node(string name, long size)
+        {
+            memory = MemoryMappedFile.CreateOrOpen(name, size);
+        }
+
+        public void Dispose()
+        {
+            memory?.Dispose();
+
+            memory = null;
+
+            GC.SuppressFinalize(this);
+        }
+
+        ~Node()
+        {
+            Dispose();
+        }
+    }
+}
